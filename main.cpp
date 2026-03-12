463b1db3e55b262dcbc79eb0d133fc1c91e9fefa
Jun <tjrwns0012@naver.com>
2026-03-12 14:02:56 +0900

    엔진 구성

--- main.cpp
+++ main.cpp
@@ -1,64 +1,90 @@
 ﻿#include <iostream>
+#include<conio.h> // 키보드
 
 using namespace std;
 
-int Pocket(int* Index, int* Boll)
-{
-	for (int i = 0; i < 45; i++)
-	{
-		Index[i] = i + 1;
-		*Boll = i + 1;
-		cout << Index[i] << " ";
-	}
-	cout << endl;
-	return *Boll;
-}
-
-
 int main()
 {
-	// 주머니에 45개의 공이 있습니다.
-	// 그 중에 6개를 뽑는 프로그램을 만들어보세요.
+	int Temp = 0;
 
-	// Initialize -> 초기화
+	int MapX = 10;
+	int MapY = 10;
 
-	int Index[45] = { 0, };
-	int Check[6] = { 0, };
-	int Boll;
-	int Count = 6;
-	int s;
+	int PlayerX = 1;
+	int PlayerY = 1;
 
-	srand((unsigned)time(nullptr));
+	int Wall[10][10] = {
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+	{ 1, 2, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+	};
 
-	Pocket(Index, &Boll);
-
-	for (int i = 0; i < Boll; i++)
+	for (;;)
 	{
-		if (i < 6)
+		for (int Y = 0; Y < MapY; Y++)
 		{
-			cin >> s;
-		}
-
-		if (i < 6)
-		{
-			if (s == *(Index + s - 1))
+			for (int X = 0; X < MapX; X++)
 			{
-				cout << *(Index + s - 1) << " ";
-			}
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "P";
+				}
 
-			else
-			{
-				cout << "값이 없습니다." << " ";
+				else if (Wall[Y][X] == 1)
+				{
+					cout << "*";
+				}
+
+				else if (Wall[Y][X] == 0)
+				{
+					cout << " ";
+				}
 			}
+			cout << endl;
 		}
 
-		if (i == 6)
+		Temp = _getch();
+
+		if (Temp == 'w')
 		{
-			break;
+			if (Wall[PlayerY-1][PlayerX] == 0)
+			{
+				PlayerY--;
+			}
 		}
+
+		if (Temp == 's')
+		{
+			if (Wall[PlayerY+1][PlayerX] == 0)
+			{
+				PlayerY++;
+			}
+		}
+
+		if (Temp == 'a')
+		{
+			if (Wall[PlayerY][PlayerX-1] == 0)
+			{
+				PlayerX--;
+			}
+		}
+
+		if (Temp == 'd')
+		{
+			if (Wall[PlayerY][PlayerX+1] == 0)
+			{
+				PlayerX++;
+			}
+		}
+
+		system("cls");
 	}
-
-	// 주머니에 45개 중 -> 6개 뽑기
-
 	return 0;
 }
